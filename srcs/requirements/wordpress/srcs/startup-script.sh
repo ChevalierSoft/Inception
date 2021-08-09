de191fdb52eb18a33b5d5c4b9399f1bb93449dd8
ChevalierSoft <ibanarashi@outlook.fr>
2021-08-09 21:42:25 +0200

    updating startup script to start phpfpm

--- srcs/requirements/wordpress/srcs/startup-script.sh
+++ srcs/requirements/wordpress/srcs/startup-script.sh
@@ -1,5 +1,5 @@
 envsubst '$MYSQL_DB_NAME $MYSQL_USER $MYSQL_USER_PASSWORD' < /tmp/wp-config.php > /var/www/html/wp-config.php
-php-fpm7
+#php-fpm7
 #nginx -g 'daemon off;'
-#php-fpm7.3 --nodaemonize
-tail -f /dev/null
+php-fpm7.3 --nodaemonize
+#tail -f /dev/null

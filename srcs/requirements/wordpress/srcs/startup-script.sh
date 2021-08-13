ca9f0638c1813be5b760bbadd43b107bc8d9db26
ChevalierSoft <ibanarashi@outlook.fr>
2021-08-13 18:10:23 +0200

    now the 2 containers are sharing there volume. todo : php not working

--- srcs/requirements/wordpress/srcs/startup-script.sh
+++ srcs/requirements/wordpress/srcs/startup-script.sh
@@ -1,5 +1,12 @@
+#because it's not working during the building phase of docker-compose
+tar -xf wordpress-4.8.15.tar.gz
+mv /wordpress/* /var/www/html/
+
+# setting the credentials
 envsubst '$MYSQL_DB_NAME $MYSQL_USER $MYSQL_USER_PASSWORD' < /tmp/wp-config.php > /var/www/html/wp-config.php
-#php-fpm7
-#nginx -g 'daemon off;'
+
+# starting the daemon
 php-fpm7.3 --nodaemonize
+
+# owo
 #tail -f /dev/null
